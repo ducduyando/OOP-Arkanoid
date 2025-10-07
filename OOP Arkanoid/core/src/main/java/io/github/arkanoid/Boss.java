9f85764521b9f28e28ec3098d7165a55aeb79c8e
Dungne1907 <dle355444@gmail.com>
2025-10-07 18:49:53 +0700

    sua loi boss

--- OOP Arkanoid/core/src/main/java/io/github/arkanoid/Boss.java
+++ OOP Arkanoid/core/src/main/java/io/github/arkanoid/Boss.java
@@ -18,12 +18,15 @@ public class Boss extends Actor {
     private int hp;
     private final int maxFrame;
     private int currentFrame = 0;
+    private float animationTimer = 0f;
+    private final float frameDuration = 0.1f;
 
     Boss(Texture texture, float x, float y) {
         this.textureRegion = new TextureRegion(texture, 0, 0, BOSS1_WIDTH, BOSS1_HEIGHT);
         this.velocityVector = new Vector(BOSS1_VELOCITY_X, BOSS1_VELOCITY_Y);
         this.hitBox = new Rectangle(x, y, BOSS1_WIDTH, BOSS1_HEIGHT);
         maxFrame = texture.getWidth() / BOSS1_WIDTH;
+        this.hp = 100;
         setPosition(x, y);
         setSize(BOSS1_WIDTH, BOSS1_HEIGHT);
         setOrigin(getWidth() / 2f, getHeight() / 2f);
@@ -39,8 +42,12 @@ public class Boss extends Actor {
 
     @Override
     public void act(float delta) {
-        if (!isDead()) {
-            currentFrame = (currentFrame + 1) / maxFrame;
+        super.act(delta);
+        if (isDead()) return;
+        animationTimer += delta;
+        if (animationTimer >= frameDuration) {
+            animationTimer -= frameDuration;
+            currentFrame = (currentFrame + 1) % maxFrame;
             this.textureRegion.setRegion(BOSS1_WIDTH * currentFrame, 0, BOSS1_WIDTH, BOSS1_HEIGHT);
         }
     }
